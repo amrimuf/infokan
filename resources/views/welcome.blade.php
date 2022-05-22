459c2565cc671f94ebc4af052cbd3b49f545d567
Amri Mufti <amrimuvti@gmail.com>
2022-05-22 15:10:17 +0700

    first

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Kelompok 5
+                    Kelompok 6
                 </div>
 
                 <div class="links">
