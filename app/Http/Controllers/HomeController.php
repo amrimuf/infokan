409f4d7093a929ab8d9826b6e92bb78d7897cb28
galihindra22 <69674716+galihindra22@users.noreply.github.com>
2022-06-05 20:30:16 +0800

    user location & map (WIP)
    yang di home.blade yang tampilan map, yang controller user location

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+// use Stevebauman\Location\Facades\Location; buat load package user location
 
 class HomeController extends Controller
 {
@@ -24,6 +25,7 @@ class HomeController extends Controller
      */
     public function index()
     {
+        // dd(Location::get('118.99.76.133')); ip buat user's location
         $resto = DB::table('users')
             ->where('is_restoran', '=', '1')
             ->paginate();
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">Daftar Restoran</div>
-
+                {{-- <iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.openstreetmap.org/export/embed.html?bbox=112.80009090900423%2C-7.290888589943765%2C112.8056162595749%2C-7.2877331992866585&amp;layer=mapnik&amp;marker=-7.289313557925458%2C112.80285358428955" style="border: 1px solid black"></iframe><br/><small></small> map tapi user location masih manual--}}
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
